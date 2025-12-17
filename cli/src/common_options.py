b02de98fd6f79f0de9580360e7c9b4df4fe84de6
Ofek Ashery <ofek.ashery@gmail.com>
2025-12-17 22:20:25 +0200

    Add common_options

---
+++ cli/src/common_options.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import typer
+
+DEFAULT_LIBS_PATH = Path("libs")
+DEFAULT_LIBS_DEP_GROUP = "libs"
+
+option_group = typer.Option(
+    default=DEFAULT_LIBS_DEP_GROUP,
+    help="Dependency group to to vendor.",
+)
+option_libs_path = typer.Option(
+    default=DEFAULT_LIBS_PATH,
+    help="Directory to vendor the dependencies into.",
+)
--- cli/src/main.py
+++ cli/src/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import typer
 
+from .common_options import option_group, option_libs_path
 from .utils import run_command
 
 app = typer.Typer(
@@ -14,19 +15,11 @@ DEFAULT_LIBS_DEP_GROUP = "libs"
 
 
 @app.command(
+    help="Vendor the isolated environment dependencies.",
     # Allow any extra args to be passed to `uv`
     context_settings={"allow_extra_args": True, "ignore_unknown_options": True},
 )
-def vendor(
-    group: str = typer.Option(
-        default=DEFAULT_LIBS_DEP_GROUP,
-        help="Dependency group to to vendor.",
-    ),
-    libs_path: Path = typer.Option(
-        default=DEFAULT_LIBS_PATH,
-        help="Directory to vendor the dependencies into.",
-    ),
-):
+def vendor(group: str = option_group, libs_path: Path = option_libs_path):
     """Install the isolated environment dependencies."""
     lock_file = libs_path / "vednor.txt"
     run_command(
@@ -59,15 +52,7 @@ def vendor(
     context_settings={"allow_extra_args": True, "ignore_unknown_options": True},
 )
 def add(
-    ctx: typer.Context,
-    group: str = typer.Option(
-        default=DEFAULT_LIBS_DEP_GROUP,
-        help="Dependency group to to vendor.",
-    ),
-    libs_path: Path = typer.Option(
-        default=DEFAULT_LIBS_PATH,
-        help="Directory to vendor the dependencies into.",
-    ),
+    ctx: typer.Context, group: str = option_group, libs_path: Path = option_libs_path
 ):
     """Add packages to the isolated environment."""
     run_command(["uv", "add", "--frozen", "--group", group, *ctx.args])
@@ -80,15 +65,7 @@ def add(
     context_settings={"allow_extra_args": True, "ignore_unknown_options": True},
 )
 def remove(
-    ctx: typer.Context,
-    group: str = typer.Option(
-        default=DEFAULT_LIBS_DEP_GROUP,
-        help="Dependency group to to vendor.",
-    ),
-    libs_path: Path = typer.Option(
-        default=DEFAULT_LIBS_PATH,
-        help="Directory to vendor the dependencies into.",
-    ),
+    ctx: typer.Context, group: str = option_group, libs_path: Path = option_libs_path
 ):
     """Remove packages from the isolated environment."""
     run_command(["uv", "remove", "--frozen", "--group", group, *ctx.args])
