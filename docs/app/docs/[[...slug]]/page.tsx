8eb0e4bb9de863167b0e8194f10047310052d9b9
Ofek Ashery <ofek.ashery@gmail.com>
2025-12-20 16:11:26 +0200

    Add landing page

--- docs/app/docs/[[...slug]]/page.tsx
+++ docs/app/docs/[[...slug]]/page.tsx
@@ -17,6 +17,11 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
 
   const MDX = page.data.body;
 
+  // If we're at the root /docs path, show the homepage content
+  if (!params.slug || params.slug.length === 0) {
+    return <MDX />;
+  }
+
   return (
     <DocsPage toc={page.data.toc} full={page.data.full}>
       <DocsTitle>{page.data.title}</DocsTitle>
@@ -38,7 +43,7 @@ export async function generateStaticParams() {
 }
 
 export async function generateMetadata(
-  props: PageProps<'/docs/[[...slug]]'>,
+  props: PageProps<'/docs/[[...slug]]'>
 ): Promise<Metadata> {
   const params = await props.params;
   const page = source.getPage(params.slug);
