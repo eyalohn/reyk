b37e6f62d554ca9d21ad47c808b72a961adf261a
Ofek Ashery <ofek.ashery@gmail.com>
2025-12-21 21:41:53 +0200

    Rename

--- pyisolate_cli/src/pyisolate_cli/consts.py
+++ pyisolate_cli/src/pyisolate_cli/consts.py
@@ -2,15 +2,15 @@ from pathlib import Path
 
 import typer
 
-DEFAULT_LIBS_PATH = Path("libs")
-DEFAULT_LIBS_DEP_GROUP = "libs"
+DEFAULT_LIBS_PATH = Path("./libs")
+DEFAULT_LIBS_DEPS_GROUP = "libs"
 
 LOCK_FILE_NAME = "vendor.lock"
 
 
 option_group = typer.Option(
-    default=DEFAULT_LIBS_DEP_GROUP,
-    help="Dependency group to to vendor.",
+    default=DEFAULT_LIBS_DEPS_GROUP,
+    help="Dependency group to to vendor.",  # https://docs.astral.sh/uv/concepts/projects/dependencies/#dependency-groups
 )
 option_libs_path = typer.Option(
     default=DEFAULT_LIBS_PATH,
