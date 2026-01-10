bb70d8c2cd45b15649ea700b9191e985832e3289
OughtToPrevail <oughttoprevail@gmail.com>
2026-01-10 14:03:41 +0200

    Added print of sys path to debug why example project import doesn't work

--- tests/blackbox/conftest.py
+++ tests/blackbox/conftest.py
@@ -44,6 +44,7 @@ def import_example_project() -> None:
     # This is crucial to happen before `clear_imported_modules_cache` as it will isolate
     # the project every test as the `__init__` will be reloaded (as its removed from sys modules)
 
+    print(sys.path)
     import example_project
 
 
