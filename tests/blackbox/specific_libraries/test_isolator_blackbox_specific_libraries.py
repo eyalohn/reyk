e3d9122f2382380a6b0eeae1fd56d0aec5cecc86
Eyal Ohn <47192648+eyalohn@users.noreply.github.com>
2026-05-18 23:32:11 +0300

    Feature/test boto3 (#21)
    Fixed absolute import edge case:
    Import of a module in a partially initialized package that isn't used yet because if it was used it would be a circular import.

--- tests/blackbox/specific_libraries/test_isolator_blackbox_specific_libraries.py
+++ tests/blackbox/specific_libraries/test_isolator_blackbox_specific_libraries.py
@@ -36,6 +36,7 @@ def install_test_libs() -> Iterable[None]:
         "pydantic",  # Rust pyd
         "aiormq",  # Lots of relative imports
         "pika",  # Large library with lots of builtins usage
+        "boto3",  # Lots of absolute imports
     ],
 )
 def test_import_library(files_manager: ExampleProjectFileManager, library_name: str) -> None:
@@ -66,9 +67,12 @@ def test_find_distributions_with_specific_libraries(
         {
             "aiormq",
             "annotated-types",
+            "boto3",
+            "botocore",
             "dnspython",
             "idna",
             "importlib_metadata",
+            "jmespath",
             "multidict",
             "opentelemetry-api",
             "opentelemetry-sdk",
@@ -80,8 +84,12 @@ def test_find_distributions_with_specific_libraries(
             "pydantic",
             "pydantic_core",
             "pymongo",
+            "python-dateutil",
+            "s3transfer",
+            "six",
             "typing-inspection",
             "typing_extensions",
+            "urllib3",
             "yarl",
             "zipp",
         },
